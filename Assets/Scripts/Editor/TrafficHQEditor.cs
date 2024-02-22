f3d84abf9910fd635d79b19b96197a1bc7adf39e
Shrimp <tpwns0429@gmail.com>
2024-02-22 16:31:08 +0900

    TrafficHQEditorcs Update
    2024-02-22 오후 4시30분 까지의 코드수정

--- Assets/Scripts/Editor/TrafficHQEditor.cs
+++ Assets/Scripts/Editor/TrafficHQEditor.cs
@@ -1,6 +1,8 @@
 using Codice.Client.Common.GameUI;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
@@ -79,5 +81,109 @@ public class TrafficHQEditor : Editor
 
         Undo.RecordObject(headquarter, "");
         headquarter.intersections.Add(trafficIntersection);
+    }
+    //씬에서 직접 설치해보도록 함
+    //Shift 
+    //Control
+    //Alt
+    private void OnSceneGUI()
+    {
+        //마우스 클릭 조작이 있었는지를 얻어옴
+        Event @event = Event.current;
+        if (@event == null)
+        {
+            return;
+        }
+        //마우스포지션 위치로 레이를 만들어줍니다.
+        Ray ray = HandleUtility.GUIPointToWorldRay(@event.mousePosition);
+        RaycastHit hit;
+        //마우스 위치로 충돌체 검출이 되었고, 마우스 클릭으로 인해 발생하였다.
+        //0 왼쪽 클릭 1 오른쪽 클릭 2는 휠버튼
+        if (Physics.Raycast(ray, out hit) &&
+            @event.type == EventType.MouseDown &&
+            @event.button == 0)
+        {
+            //마우스 왼쪽 클릭 + Shift -> 웨이포인트 추가.
+            if (@event.shift)
+            {
+                //구간없는 웨이포인트는 존재할 수 없습니다.
+                if (headquarter.curSegment == null)
+                {
+                    Debug.LogWarning("세그먼트 먼저 만들어주세요");
+                    return;
+                }
+                EditorHelper.BeginUndoGruop("Add WayPoint", headquarter);
+                AddWayPoint(hit.point);
+                Undo.CollapseUndoOperations(Undo.GetCurrentGroup());
+            }
+            //마우스 왼족 클릭 + Control -> 세그먼트 추가
+            //첫번째 웨이포인트도 같이 추가됌
+            else if (@event.control)
+            {
+                EditorHelper.BeginUndoGruop("Add Segment", headquarter);
+                AddSegment(hit.point);
+                AddWayPoint(hit.point);
+                Undo.CollapseUndoOperations(Undo.GetCurrentGroup());
+            }
+            //마우스 왼쪽 클릭 + Alt -> 인터섹션 추가
+            else if (@event.alt)
+            {
+                EditorHelper.BeginUndoGruop("Add Intersection", headquarter);
+                AddIntersection(hit.point);
+                Undo.CollapseUndoOperations(Undo.GetCurrentGroup());
+            }
+        }
+        //웨이포인트 시스템을 히어라키뷰에서 선택한 게임 객체로 설정
+        Selection.activeGameObject = headquarter.gameObject;
+        //선택한 웨이포인트를 처리함
+        if (lastWaypoint != null)
+        {
+            //레이가 충돌할 수 있도록 Plane을 사용함
+            Plane plane = new Plane(Vector3.up, lastWaypoint.GetVisualPos());
+            plane.Raycast(ray, out float dst);
+            Vector3 hitpoint = ray.GetPoint(dst);
+            //마우스 버튼을 처음 눌렀을때, LastPoint 재설정
+            if (@event.type == EventType.MouseDown && @event.button == 0)
+            {
+                lastPoint = hitpoint;
+                startPosition = lastWaypoint.transform.position;
+            }
+            if (@event.type == EventType.MouseDrag && @event.button == 0)
+            {
+                Vector3 realPos = new Vector3(hitpoint.x - lastPoint.x, 0, hitpoint.z - lastPoint.z);
+                lastWaypoint.transform.position += realPos;
+                lastPoint = hitpoint;
+
+            }
+            //선택한 웨이포인트를 해제
+            if (@event.type == EventType.MouseUp && @event.button == 0)
+            {
+                Vector3 curPos = lastWaypoint.transform.position;
+                lastWaypoint.transform.position = startPosition;
+                Undo.RegisterFullObjectHierarchyUndo(lastWaypoint, "Move WayPoint");
+                lastWaypoint.transform.position = curPos;
+            }
+            //구 하나 그리기
+            Handles.SphereHandleCap(0, lastWaypoint.GetVisualPos(), Quaternion.identity,
+                headquarter.waypointSize * 2f, EventType.Repaint);
+            HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Passive));
+            SceneView.RepaintAll();
+        }
+        // 모든 웨이포인트로 부터 판별식을 통해 충돌되는 웨이포인트를 LastWayPoint로 세팅
+        if (lastWaypoint == null)
+        {
+            lastWaypoint = headquarter.GetAllWaypoints().FirstOrDefault
+                (i => EditorHelper.SphereHit(i.GetVisualPos(), headquarter.waypointSize, ray));
+        }
+        //HQ의 현재 수정중인 세그먼트를 현재 세그먼트를 현재 선택한 세그먼트로 대체함
+        if(lastWaypoint != null && @event.type == EventType.MouseDown)
+        {
+            headquarter.curSegment = lastWaypoint.segment;
+        }
+        //현재 웨이포인트를 재설정. 마우스를 이동하면 선택이 풀리도록
+        else if(lastWaypoint != null && @event.type == EventType.MouseMove)
+        {
+            lastWaypoint = null;
+        }
     }
 }
